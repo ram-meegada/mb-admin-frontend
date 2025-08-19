ffc36514c9bb431de9c78f5d4902c86b64f8be89
ram-meegada <ramumeegada1811@gmail.com>
2025-08-19 21:26:34 +0530

    changed base url

--- src/utils/endpoints.ts
+++ src/utils/endpoints.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = import.meta.env.VITE_LOCAL_BASE_URL
+export const BASE_URL = import.meta.env.VITE_BASE_URL
 
 // export const BASE_URL = "https://mb-business-backend.onrender.com/"
 
